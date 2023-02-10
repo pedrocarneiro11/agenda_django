3f20e60b5ed6b9768c36d04f65b17ed745440dd4
Pedro <pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.comgit config --global user.email pedrocarneirocunha87@gmail.com>
2023-02-10 15:06:03 -0300

    criando as rotas de login e logout

--- core/views.py
+++ core/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core.models import Evento
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
 # Create your views here.
@@ -12,9 +14,28 @@ def login_user(request):
     return render(request, 'login.html')
 
 
+def logout_user(request):
+    logout(request)
+    return redirect('login/')
+
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+        else:
+            messages.error(request, "Usuario e/ou senha invalidos")
+            return redirect('/login/')
+    return redirect('/')
+
+
 @login_required(login_url='login/')
 def lista_eventos(request):
     usuario = request.user
-    evento = Evento.objects.all()
+    evento = Evento.objects.filter(usuario=usuario)
     dados = {'eventos': evento}
     return render(request, 'agenda.html', dados)
